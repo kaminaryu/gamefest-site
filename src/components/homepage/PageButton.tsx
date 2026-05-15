4fd8bc3625941d73dd9b1c0a867335e63d07a8d4
IzzAmani <izzryugasaki@gmail.com>
2026-05-15 21:56:42 +0800

    Change Button Size

--- src/components/homepage/PageButton.tsx
+++ src/components/homepage/PageButton.tsx
@@ -2,7 +2,7 @@ export default function PageButton({href, src}: {href: string, src: string}) {
     return (
             <a 
                 href={href}
-                className="fixed right-5 md:right-20 bottom-5 md:bottom-10 w-16 md:w-24 h-auto p-3 rounded-[10vh] z-10
+                className="fixed right-5 md:right-20 bottom-5 md:bottom-10 w-16 md:w-20 h-auto p-3 rounded-[10vh] z-10
                            bg-gradient-to-r from-neon-purple to-neon-cyan hover:brightness-125 opacity-75"
             >
                 <img
